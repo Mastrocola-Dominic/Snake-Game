207a322e7a68aad4af91153c34fcac0fe41556b3
Mastrocola-Dominic <dommastroc@gmail.com>
2019-08-09 20:54:04 -0400

    Update snake.js
    Added
    -Restricted movement so the snake cannot move in the opposite direction it is currently moving.
    -When the snake collides with the apple it adds length to the snake and gives the apple a new position
    -When the snake hits the edge of itself it resets.

--- snake.js
+++ snake.js
@@ -3,47 +3,86 @@ function Snake(x, y, dx, dy, canvas) {
   this.y = y;
   this.dx = dx;
   this.dy = dy;
-  this.size = 10;
+  this.size = 20;
   this.canvas = canvas;
   this.lastUpdate = 0;
-
+  this.body = [[]];
+  this.length = 0;
   //set direction
-  this.setDir = function(x, y) {
-    console.log(x, y);
-    this.dx = x;
-    this.dy = y;
+  this.setDir = function(dx, dy) {
+    if(dx !== 0) {
+      if(this.dx === 0) {
+        this.dx = dx;
+        this.dy = 0;
+      }
+    }
+    else {
+      if(this.dy === 0) {
+        this.dx = 0;
+        this.dy = dy;
+      }
+    }
+
   }
   //update position
   this.update = function(timestamp) {
-    if(timestamp - this.lastUpdate >= 100) {
+
+    if(timestamp - this.lastUpdate >= 50) {
+      for(let i = 0; i < this.length; i++) {
+        this.body[i] = this.body[i+1];
+      }
+      this.body[this.length - 1] = [this.x, this.y];
       this.lastUpdate = timestamp;
+
       this.x += this.dx * this.size;
       this.y += this.dy * this.size;
       this.checkCollisions();
-      console.log('update');
     }
   }
   //draw the snake
   this.draw = function(ctx) {
     ctx.fillStyle = '#fff';
+    ctx.strokeStyle = '#000';
     ctx.fillRect(this.x, this.y, this.size, this.size);
+    ctx.strokeRect(this.x, this.y, this.size, this.size);
+    for(let i = 0; i < this.length; i++) {
+      ctx.fillRect(this.body[i][0], this.body[i][1], this.size, this.size);
+      ctx.strokeRect(this.body[i][0], this.body[i][1], this.size, this.size);
+    }
   }
   //check for collisions
   this.checkCollisions = function() {
+    for(let i = 0; i < this.length; i++) {
+      if(this.x === this.body[i][0] && this.y === this.body[i][1]) {
+        this.death();
+      }
+    }
 
     if(this.x < 0) {
-      this.x = 0;
+      this.death();
     }
     if(this.x + this.size > canvas.width) {
-      this.x = canvas.width - this.size;
+      this.death();
     }
     if(this.y + this.size > canvas.height) {
-      this.y = canvas.height - this.size;
+      this.death();
     }
     if(this.y < 0) {
-      this.y = 0;
-    }if(this.x === apple.x && this.y === apple.y) {
-      alert('Apple has been eaten');
+      this.death();
     }
+    if(this.x === apple.x && this.y === apple.y) {
+      apple.getNewPos();
+      this.body[this.length] = [this.x, this.y];
+      this.body.push([]);
+      this.length++;
+    }
+  }
+  this.death = function() {
+    this.body = [[]];
+    this.length = 0;
+    this.dx = 1;
+    this.dy = 0;
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
   }
 }
